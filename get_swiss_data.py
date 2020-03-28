62d8a4aa5fb3951b4b432588739ec1d0eb585335
ENRISCOL <enrica.scolari@axpo.com>
2020-03-28 13:21:19 +0100

    data for switzerland

---
+++ get_swiss_data.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import os
+
+directory = r'C:\Users\ENRISCOL\AAVsCovid\covid19-cases-switzerland'
+
+swiss_data = pd.DataFrame()
+for filename in os.listdir(directory):
+    if filename.endswith("openzh.csv"):
+        df = pd.read_csv(os.path.join(directory, filename)).set_index("Date")
+        df.columns = [filename[8:-22] + str(col) for col in df.columns]
+        swiss_data = pd.concat([swiss_data, df], axis=1)
+    else:
+        continue
