c769215e60eaad4a4059f6a39bc861656f911ae0
Valentin Julia <Valentin.Julia@axpo.group>
2020-03-28 15:08:27 +0100

    fix bug when loading swiss data

--- data/get_swiss_data.py
+++ data/get_swiss_data.py
@@ -3,14 +3,15 @@ import os
 from pathlib import Path
 
 swiss_data = pd.DataFrame()
-directory = Path(__file__).parent
-for filename in os.listdir(directory / "covid19-cases-switzerland"):
+directory = Path(__file__).parent / "covid19-cases-switzerland"
+
+for filename in os.listdir(directory):
     if filename.endswith("openzh.csv"):
-        df = pd.read_csv(os.path.join(directory, filename)).set_index("Date")
+        df = pd.read_csv(directory / filename).set_index("Date")
         df.columns = [filename[8:-22] + str(col) for col in df.columns]
         swiss_data = pd.concat([swiss_data, df], axis=1)
 
     else:
         continue
 
-swiss_data.to_csv("swiss_data.csv", index=False)
+swiss_data.to_csv("swiss_data.csv")
